b7532c8b816bd3087340a12a7fda78124155cd64
Coen Mathijssen <mathijssen.coen@gmail.com>
2019-11-13 12:58:02 +0100

    Finished cleaning my data with a conversion ratio of 83%

--- project/src/main.js
+++ project/src/main.js
@@ -29,8 +29,6 @@ runQuery(url, query)
 let self = this
 
 function runQuery (url, query) {
-  // Test if the endpoint is up and print result to page
-  fetch(url)
   // Call the url with the query attached, output data
   fetch(url + '?query=' + encodeURIComponent(query) + '&format=json')
     .then(res => res.json())
@@ -52,21 +50,8 @@ function runQuery (url, query) {
     })
 }
 
-
-
 function cleanDataYear (fetchedData) {
-  // fetchedData.forEach((item, count) => {
-  //   let itemDate = item.date
-  //   let itemDateValue = itemDate.value
-
-  //   // Transform all elements to uppercase
-  //   itemDateValue = itemDateValue.toUpperCase()
-
-  //   // Replace all unnecessary characters with an empty string
-  //   itemDateValue = cleanCharacter(itemDate)
-  //   // console.log(itemDateValue)
-  // })
-  const newData = fetchedData.map(item => {
+  let newData = fetchedData.map(item => {
     let itemDateValue = item.date.value
 
     // Transform all elements to uppercase
@@ -78,57 +63,169 @@ function cleanDataYear (fetchedData) {
     // Replace all unnecessary characters with an empty string
     item.date.value = cleanCharacter(item)
 
+    // Replace all 'eeuwen' en 'centuries' with an empty string
+    item = replaceCenturies(item)
+
+    // Clean data if they have this format: 12-03-1990, or this format: 1900-2000. Returns the (average) year
+    item = convertToYear(item)
+
+    // Convert all strings to numbers
+    item = convertToNumber(item)
+
     return {
       id: item.choCount,
       date: {
         type: item.date.type,
         value: item.date.value,
         bc: item.date.bc,
-        ad: item.date.ad
+        ad: item.date.ad,
+        century: item.date.century
       },
       landLabel: item.landLabel
     }
   })
 
-  // newData.forEach(item => console.log('items: ', item.date.bc))
+  // delete all items which don't fit the format by now
+  let finalArray = deleteUnformattedData(newData)
+  console.log('finalArray: ', finalArray)
+
+  // finalArray.forEach(item => console.log('items: ', item.date.value))
+
+  return finalArray
+}
+
+function deleteUnformattedData (array) {
+  const finalArray = array.filter(item => {
+    if (item.date.value.toString().length === 4) {
+      return item
+    }
+  })
+  return finalArray
 }
 
 function cleanCharacter (item) {
   let itemDateValue = item.date.value
-  // console.log(itemDateValue)
 
-  // Replace the dot, (, and )
+  // Replace the dot, (, ) and /
   itemDateValue = replaceWithWhichString(itemDateValue, /\./g)
   itemDateValue = replaceWithWhichString(itemDateValue, /[()]/g)
   itemDateValue = replaceWithWhichString(itemDateValue, /[()]/g)
   itemDateValue = replaceWithWhichString(itemDateValue, /\s/g)
   itemDateValue = replaceWithWhichString(itemDateValue, /\?/g)
+  itemDateValue = replaceWithWhichString(itemDateValue, /\//g, '-')
 
-  // // Replace all left letters
-  // itemDateValue = replaceWithWhichString(itemDateValue, /([a-zA-Z ])/g)
+  // Replace all left letters with an empty string
+  itemDateValue = replaceWithWhichString(itemDateValue, /([a-zA-Z ])/g)
+
   return itemDateValue
 }
 
-function replaceWithWhichString (item, specialCharacter) {
-  return item.replace(specialCharacter, '')
+function replaceWithWhichString (item, specialCharacter, replacedString = '') {
+  return item.replace(specialCharacter, replacedString)
 }
 
 function replaceChrist (item) {
   let itemDate = item.date
   if (itemDate.value.includes('BC')) {
-    // console.log(item)
     itemDate.bc = true
     itemDate.value = itemDate.value.replace('BC', '')
-  } else {
-    itemDate.bc = false
   }
   if (itemDate.value.includes('AD')) {
     itemDate.ad = true
     itemDate.value = itemDate.value.replace('AD', '')
+  } else {
+    itemDate.bc = false
+    itemDate.ad = false
   }
   item.date = itemDate
   return item
 }
 
-// let fetchedData = runQuery(url, query)
-// console.log(fetchedData)
+function replaceCenturies (item) {
+  let itemDate = item.date
+  if (itemDate.value.includes('EEEUW') || itemDate.value.includes('EEUW') || itemDate.value.includes('CENTURY')) {
+    itemDate.value = itemDate.value.replace('EEEUW', '')
+    itemDate.value = itemDate.value.replace('EEUW', '')
+    itemDate.value = itemDate.value.replace('CENTURY', '')
+    itemDate.century = true
+  } else if (itemDate.value.includes('TH')) {
+    itemDate.value = itemDate.value.replace(/\t.*/, '')
+    itemDate.century = true
+  } else {
+    itemDate.century = false
+  }
+  item.date = itemDate
+  // console.log(itemDate)
+  return item
+}
+
+function convertToYear (item) {
+  let itemDateValue = item.date.value
+
+  if (itemDateValue.length === 7) {
+    let splittedArray = itemDateValue.split('-')
+    // Check if the array has 3 items, only contain numbers and if the last item in the array is a year
+    if (splittedArray[0] && splittedArray[1] && splittedArray[0].match(/^[0-9]+$/) != null && splittedArray[1].match(/^[0-9]+$/) != null) {
+      if (splittedArray[1].length === 4) {
+        item.date.value = splittedArray[0]
+      }
+    }
+  }
+
+  if (itemDateValue.length === 8) {
+    let splittedArray = itemDateValue.split('-')
+    // Check if the array has 3 items, only contain numbers and if the last item in the array is a year
+    if (splittedArray[0] && splittedArray[1] && splittedArray[2] && splittedArray[0].match(/^[0-9]+$/) != null && splittedArray[1].match(/^[0-9]+$/) != null && splittedArray[2].match(/^[0-9]+$/) != null) {
+      if (splittedArray[2].length === 4) {
+        item.date.value = splittedArray[2]
+      }
+    }
+  }
+
+  if (itemDateValue.length === 9) {
+    let splittedArray = itemDateValue.split('-')
+    // Check if first array item is a year and only contains numbers
+    if (splittedArray[0] && splittedArray[1] && splittedArray[0].match(/^[0-9]+$/) != null && splittedArray[1].match(/^[0-9]+$/) != null) {
+      if (splittedArray[0].length === 4 && splittedArray[1].length === 4) {
+        item.date.value = splitStringCalcAverage(itemDateValue)
+      }
+    }
+
+    // Check if the array has 3 items, only contain numbers and if the last item in the array is a year
+    if (splittedArray[0] && splittedArray[1] && splittedArray[2] && splittedArray[0].match(/^[0-9]+$/) != null && splittedArray[1].match(/^[0-9]+$/) != null && splittedArray[2].match(/^[0-9]+$/) != null) {
+      if (splittedArray[2].length === 4) {
+        item.date.value = splittedArray[2]
+      }
+    }
+  }
+
+  if (itemDateValue.length === 10) {
+    let splittedArray = itemDateValue.split('-')
+    if (splittedArray[2] && splittedArray[2].length === 4) {
+      // console.log(item.date.value, splittedArray[2])
+      item.date.value = splittedArray[2]
+    } // Check if first array item is a year and only contains numbers
+    if (splittedArray[0].length === 4 && splittedArray[0].match(/^[0-9]+$/) != null) {
+      item.date.value = splittedArray[0]
+    }
+  }
+  return item
+}
+
+// Merge two array
+function splitStringCalcAverage (item) {
+  let splittedArray = item.split('-')
+  return average(splittedArray[0], splittedArray[1])
+}
+// Wiebe helped me with this function
+function average (a, b) {
+  // force the input as numbers *1
+  return Math.round(((a * 1 + b * 1) / 2))
+}
+
+// Convert all left strings to number
+function convertToNumber (item) {
+  let itemDateValue = item.date.value
+  item.date.value = parseInt(itemDateValue)
+  return item
+}
